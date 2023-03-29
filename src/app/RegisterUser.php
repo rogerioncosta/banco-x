40dcc779e196a460f1b9acb835a0946f09bef189
Rogério Costa <rogerioncosta1@gmail.com>
2023-03-28 21:06:18 -0300

    Criando serviço para registrar usuários

--- src/app/RegisterUser.php
+++ src/app/RegisterUser.php
@@ -2,7 +2,24 @@
 
 declare(strict_types=1); // A tipagem deve ser respeitada
 
-class RegisterUser
-{
-    
+use BancoX\repositories\user\IUserRepository;
+
+// Vai unir as entidades, solitações do usuário com os repositórios. Vai chamar os repositórios.
+class RegisterUser {
+    public IUserRepository $repository;
+
+    public function __construct(IUserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(string $name, string $email, string $password, string $cpf): mixed {
+        $this->repository->createUser($name, $email, $password, $cpf);
+    }
 }
+
+$inMemoryRepository = new UserRepositoryInMemory();
+
+$registerSaveInMemory = new RegisterUser($inMemoryRepository);
+
+$registerSaveInMemory->execute('Rogerio', 'rogerio@gmail.com', '123456A*', '123.554.988-25');
