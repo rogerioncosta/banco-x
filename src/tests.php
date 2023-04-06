82e18fcd7242aa273b6efc775bfdbffe3e2657e5
Rogério Costa <rogerioncosta1@gmail.com>
2023-04-06 18:06:32 -0300

    incluindo arquivo de teste

---
+++ src/tests.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BancoX\tests;
+
+use BancoX\repositories\user\UserRepositoryInMemory;
+use BancoX\app\RegisterUser;
+use BancoX\app\GetAllUsers;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+// Teste:
+
+// Cria usuário (cadastro)
+$inMemoryRepository = new UserRepositoryInMemory();
+
+$registerSaveInMemory = new RegisterUser($inMemoryRepository);
+
+$registerSaveInMemory->execute('Rogerio', 'rogerio@gmail.com', 'Senha@12345678', '123.554.988-25');
+
+// Obter todos os usuários
+$getAllUsersInMemory = new GetAllUsers($inMemoryRepository);
+
+print_r($getAllUsersInMemory->execute());
