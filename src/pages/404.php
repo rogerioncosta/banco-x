93397a35c86d43928d654db766cae1b68ba7fac1
Rogério Costa <rogerioncosta1@gmail.com>
2023-04-18 12:27:38 -0300

    adiciona h1

--- src/pages/404.php
+++ src/pages/404.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . "/pages/partials/head.php";
+require_once __DIR__ . "/partials/head.php";
 
 useHead('Página não econtrada');
 
-require_once __DIR__ . "/pages/partials/header.php";
+require_once __DIR__ . "/partials/header.php";
  
 ?>
-
+<h1>404</h1>
 <p>Nada encontrado</p>
 
-<?php require_once __DIR__ . "/pages/partials/footer.php"; ?>
+<?php require_once __DIR__ . "/partials/footer.php"; ?>
