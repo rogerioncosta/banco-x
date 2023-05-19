eb8ae370562144f4db051e0d905cca32f70408c2
Rogerio Costa <rogerioncosta1@gmail.com>
2023-05-19 15:38:51 -0300

    incluindo link bootstrap

--- src/pages/partials/head.php
+++ src/pages/partials/head.php
@@ -7,7 +7,9 @@ function useHead(string $title = 'Banco X', string $description = 'Sistema Banco
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="'. $description .'">
-        '. $custom .'      
+        '. $custom . '
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+              
     </head>
     ';
 }
